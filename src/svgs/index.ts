068bea21df4df291d6fa0b4131fdf4fcc4f4a19d
ehsan <m.ehsan1381@gmail.com>
2025-08-14 11:50:50 +0330

    update svgs

--- src/svgs/index.ts
+++ src/svgs/index.ts
@@ -1,3 +1,4 @@
 export { default as MenuSVG } from "./menu";
 export { default as CupSVG } from "./cup";
-export { default as avatar1 } from "./avatar1";
+export { default as Avatar1SVG } from "./avatar1";
+export { default as UserSVG } from "./user";

6c2041c1a3aa0e5fd5ae7a55c8d1363ae75689df
ehsan <m.ehsan1381@gmail.com>
2025-08-17 20:40:50 +0330

    fix login page routing

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -7,22 +7,29 @@ import {
 } from "@/components/customs";
 import { EyeSVG } from "@/svgs";
 import CupSVG from "@/svgs/cup";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const SignIn: React.FC = () => {
+  const router = useRouter();
+
   return (
     <div className="px-6 py-10 flex justify-between flex-col h-screen">
       <div>
         <p className="text-[2rem] text-center">Sign In</p>
 
         <div className="flex flex-col gap-2 mt-[5.125rem]">
-          <CustomInput
-            label="User Name"
-            isError={true}
-            helperText="rewre"
-            startIcon={<EyeSVG />}
-            id="1"
-          />
-          <CustomPassInput label="Password" id="2" />
+          <CustomInput label="User Name" id="1" />
+
+          <div>
+            <CustomPassInput label="Password" id="2" />
+            <Link
+              href="/forget-password"
+              className="!text-[#5CF8FD] text-[1.125rem]"
+            >
+              Forgot your pass?
+            </Link>
+          </div>
         </div>
       </div>
 
@@ -31,7 +38,7 @@ const SignIn: React.FC = () => {
           Sign In
         </CustomButton>
         <CustomButton
-          onClick={() => console.log("first")}
+          onClick={() => router.push("/sign-up")}
           startIcon={<CupSVG />}
         >
           Sign Up
